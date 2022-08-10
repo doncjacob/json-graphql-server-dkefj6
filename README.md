367f5c415ff3fdd2257f8d59aae8f25dbbd9d6cb
doncjacob <doncjacob@gmail.com>
2022-08-10 11:44:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# json-graphql-server-dkefj6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/json-graphql-server-dkefj6)
---
+++ db.js
@@ -0,0 +1,10 @@
+module.exports = {
+  body: [
+    { id: 1, type: 'loan app', url: 'https://cls.salesforce.com/loans/a12390jnwkl023', label: 'Loan ID: a12390jnwkl023', user_id: 123 },
+    { id: 2, type: 'sap', url: '"https://lmp.happymoney.com/loans/a12390jnwkl023', label: 'Skip-a-Pay for Loan ID: a12390jnwkl023', user_id: 456 }
+  ],
+  users: [
+    { id: 123, name: 'John Doe', email: 'john@happymoney.com' }, 
+    { id: 456, name: 'Jane Doe', email: 'jane@happymoney.com' }
+  ]
+};
